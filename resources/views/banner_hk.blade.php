dfd8889857cc1910ddda35f26c4f0a67aaf3f761
GOD OF THUNDER <avitaind@gmail.com>
2021-06-10 19:03:15 +0530

    liber v

--- resources/views/banner_hk.blade.php
+++ resources/views/banner_hk.blade.php
@@ -46,7 +46,6 @@
        
           <ol class="carousel-indicators">
            <li data-target="#home_banner" data-slide-to="0" class="active"></li>
-           <li data-target="#home_banner" data-slide-to="1"></li> 
            
         </ol>
     </div>
